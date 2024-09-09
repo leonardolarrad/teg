b04f4220b1ecccb2f59be254a61e95031c20f706
Leonardo Larrad <leonardolarrad@gmail.com>
2024-09-09 18:22:32 -0400

    Fix non-empty containers issue

--- tests/src/test_vector/test_vector.cc
+++ tests/src/test_vector/test_vector.cc
@@ -1,30 +1,74 @@
-#include <array>
 #include <vector>
-#include <deque>
-#include <forward_list>
-#include <list>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <stack>
-#include <queue>
 #include <string>
+#include <array>
 
 #include <iostream>
 #include "teg/teg.h"
 #include "test/test.h"
 
-TEST_CASE("De/serialize std::vector") {
-    {
+TEST_CASE("Trivial de/serialization") {    
+    teg::buffer b;
+    std::vector<int> v0 = { 1, 2, 3, 4, 5 };
+    teg::serialize(b, v0).or_throw();
+
+    std::vector<int> v1;
+    teg::deserialize(b, v1).or_throw();
+    
+    ASSERT_EQ(std::memcmp(v0.data(), v1.data(), v0.size() * sizeof(int)), 0);
+    ASSERT(v0 == v1);
+}
+
+TEST_CASE("Complex de/serialization") {
+    SECTION("Vector of vectors") {
         teg::buffer b;
-        std::vector<int> v0 = { 1, 2, 3, 4, 5 };
+        std::vector<std::vector<int>> v0 = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 } };
         teg::serialize(b, v0).or_throw();
 
-        std::vector<int> v1;
+        std::vector<std::vector<int>> v1;
         teg::deserialize(b, v1).or_throw();
         
-        ASSERT_EQ(std::memcmp(v0.data(), v1.data(), v0.size() * sizeof(int)), 0);
+        ASSERT(v0 == v1);
+    }
+    SECTION("Vector of vectors of vectors") {
+        teg::buffer b;
+        std::vector<std::vector<std::vector<int>>> v0 = { 
+            { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 } }, 
+            { { 11, 12, 13, 14, 15 }, 
+            { 16, 17, 18, 19, 20 } } };
+        teg::serialize(b, v0).or_throw();
+
+        std::vector<std::vector<std::vector<int>>> v1;
+        teg::deserialize(b, v1).or_throw();
+        
+        ASSERT(v0 == v1);
+    }
+    SECTION("Vector of non-trivial elements") {
+        struct non_trivial_a {
+            std::string s0 = "Text 0";
+            std::string s1 = "Text 1"; 
+            std::string s3 = "Text 2"; 
+
+            bool operator==(const non_trivial_a&) const = default;
+        };
+        
+        struct non_trivial_b {
+            std::array<std::vector<non_trivial_a>, 2> v0 = { { { non_trivial_a{} }, { non_trivial_a{} } } };
+            std::array<std::vector<non_trivial_a>, 2> v1 = { { { non_trivial_a{} }, { non_trivial_a{} } } };
+
+            non_trivial_a a0 = non_trivial_a{};
+            non_trivial_a a1 = non_trivial_a{};
+            non_trivial_a a2 = non_trivial_a{};
+
+            bool operator==(const non_trivial_b&) const = default;
+        };
+
+        teg::buffer b;
+        std::vector<non_trivial_b> v0 = { non_trivial_b{}, non_trivial_b{} };
+        teg::serialize(b, v0).or_throw();
+
+        std::vector<non_trivial_b> v1;
+        teg::deserialize(b, v1).or_throw();
+        
         ASSERT(v0 == v1);
     }
 }
