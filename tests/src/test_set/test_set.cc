9c6a7ef4729bd955c53e09029cc842885e51bee0
Leonardo Larrad <leonardolarrad@gmail.com>
2024-09-11 10:36:43 -0400

    Add more test cases to test `set`

--- tests/src/test_set/test_set.cc
+++ tests/src/test_set/test_set.cc
@@ -1,15 +1,66 @@
 #include <set>
+#include <unordered_set>
+#include <string>
 
-#include <iostream>
 #include "teg/teg.h"
 #include "test/test.h"
 
-TEST_CASE("Trivial de/serialization") {    
-    teg::buffer b;
-    std::set<int> s0 = { 1, 2, 3, 4, 5 };
-    teg::serialize(b, s0).or_throw();
+TEST_CASE("Trivial set de/serialization") {
+    SECTION("Set") {
+        teg::buffer b;
+        std::set<int> s0 = { 1, 2, 3, 4, 5 };
+        teg::serialize(b, s0).or_throw();
 
-    std::set<int> s1;
-    teg::deserialize(b, s1).or_throw();
-    ASSERT(s0 == s1);
+        std::set<int> s1;
+        teg::deserialize(b, s1).or_throw();
+        ASSERT(s0 == s1);
+    }
+    SECTION("Multiset") {
+        teg::buffer b;
+        std::multiset<int> s0 = { 1, 1, 2, 2, 5 };
+        teg::serialize(b, s0).or_throw();
+
+        std::multiset<int> s1;
+        teg::deserialize(b, s1).or_throw();
+        ASSERT(s0 == s1);
+    }
+    SECTION("Unordered set") {
+        teg::buffer b;
+        std::unordered_set<int> s0 = { 1, 2, 3, 4, 5 };
+        teg::serialize(b, s0).or_throw();
+
+        std::unordered_set<int> s1;
+        teg::deserialize(b, s1).or_throw();
+        ASSERT(s0 == s1);
+    }
+}
+
+TEST_CASE("Non-trivial set de/serialization") {
+    SECTION("Set") {
+        teg::buffer b;
+        std::set<std::string> s0 = { "a", "b", "c", "d", "e" };
+        teg::serialize(b, s0).or_throw();
+
+        std::set<std::string> s1;
+        teg::deserialize(b, s1).or_throw();
+        ASSERT(s0 == s1);
+    }
+    SECTION("Multiset") {
+        teg::buffer b;
+        std::multiset<std::string> s0 = { "a", "b", "c", "d", "e" };
+        teg::serialize(b, s0).or_throw();
+
+        std::multiset<std::string> s1;
+        teg::deserialize(b, s1).or_throw();
+        ASSERT(s0 == s1);
+    }
+    SECTION("Unordered set") {
+        teg::buffer b;
+        std::unordered_set<std::string> s0 = { "a", "b", "c", "d", "e" };
+        teg::serialize(b, s0).or_throw();
+
+        std::unordered_set<std::string> s1;
+        teg::deserialize(b, s1).or_throw();
+        ASSERT(s0 == s1);
+    }
 }
