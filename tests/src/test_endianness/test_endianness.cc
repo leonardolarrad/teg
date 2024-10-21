bd1bd8e7ecc09bf0190059b5b9eca58e5d084807
Leonardo Larrad <leonardolarrad@gmail.com>
2024-10-21 18:00:09 -0400

    Add test `endianness`

---
+++ tests/src/test_endianness/test_endianness.cc
@@ -0,0 +1,15 @@
+#include <array>
+#include <cstring>
+#include <cstdint>
+#include <iostream>
+#include <bit>
+#include <array>
+#include <bitset>
+#include <optional>
+#include <variant>
+#include <concepts>
+#include <string>
+#include <tuple>
+
+#include "teg/teg.h"
+#include "test/test.h"
