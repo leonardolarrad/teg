975ca697966f661172be48dede4c9ac42938fe93
Leonardo Larrad <leonardolarrad@gmail.com>
2024-12-22 19:37:03 -0400

    Add benchmarks `obj3d_msgpack` and `obj3d_teg_compact`

---
+++ benchmarking/src/test_obj3d/benchmark_msgpack/obj3d_msgpack.cc
@@ -0,0 +1,138 @@
+#include "teg/teg.h"
+#include "benchmarking/test_obj3d.h"
+#include "benchmark/benchmark.h" // Google benchmark
+
+#include "msgpack.hpp" // MessagePack
+
+namespace msgpack {
+MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS) {
+namespace adaptor {
+    
+template <>
+struct pack<benchmarking::test_obj3d::ivec3> {
+    template <typename Stream>
+    packer<Stream>& operator()(msgpack::packer<Stream>& o, const benchmarking::test_obj3d::ivec3& v) const {
+        o.pack_array(3);
+        o.pack(v.x);
+        o.pack(v.y);
+        o.pack(v.z);
+        return o;
+    }
+};
+
+template <>
+struct convert<benchmarking::test_obj3d::ivec3> {
+    msgpack::object const& operator()(msgpack::object const& o, benchmarking::test_obj3d::ivec3& v) const {
+        if (o.type != msgpack::type::ARRAY) { throw msgpack::type_error(); }
+        if (o.via.array.size != 3) { throw msgpack::type_error(); }
+        v.x = o.via.array.ptr[0].as<int64_t>();
+        v.y = o.via.array.ptr[1].as<int64_t>();
+        v.z = o.via.array.ptr[2].as<int64_t>();
+        return o;
+    }
+};
+
+template <>
+struct pack<benchmarking::test_obj3d::fvec3> {
+    template <typename Stream>
+    packer<Stream>& operator()(msgpack::packer<Stream>& o, const benchmarking::test_obj3d::fvec3& v) const {
+        o.pack_array(3);
+        o.pack(v.x);
+        o.pack(v.y);
+        o.pack(v.z);
+        return o;
+    }
+};
+
+template <>
+struct convert<benchmarking::test_obj3d::fvec3> {
+    msgpack::object const& operator()(msgpack::object const& o, benchmarking::test_obj3d::fvec3& v) const {
+        if (o.type != msgpack::type::ARRAY) { throw msgpack::type_error(); }
+        if (o.via.array.size != 3) { throw msgpack::type_error(); }
+        v.x = o.via.array.ptr[0].as<double>();
+        v.y = o.via.array.ptr[1].as<double>();
+        v.z = o.via.array.ptr[2].as<double>();
+        return o;
+    }
+};
+
+template <>
+struct pack<benchmarking::test_obj3d::face> {
+    template <typename Stream>
+    packer<Stream>& operator()(msgpack::packer<Stream>& o, const benchmarking::test_obj3d::face& v) const {
+        o.pack_array(2);
+        o.pack(v.vertex_index);
+        o.pack(v.normal_index);
+        return o;
+    }
+};
+
+template <>
+struct convert<benchmarking::test_obj3d::face> {
+    msgpack::object const& operator()(msgpack::object const& o, benchmarking::test_obj3d::face& v) const {
+        if (o.type != msgpack::type::ARRAY) { throw msgpack::type_error(); }
+        if (o.via.array.size != 2) { throw msgpack::type_error(); }
+        v.vertex_index = o.via.array.ptr[0].as<benchmarking::test_obj3d::ivec3>();
+        v.normal_index = o.via.array.ptr[1].as<benchmarking::test_obj3d::ivec3>();
+        return o;
+    }
+};
+
+template <>
+struct pack<benchmarking::test_obj3d::obj_3d> {
+    template <typename Stream>
+    packer<Stream>& operator()(msgpack::packer<Stream>& o, const benchmarking::test_obj3d::obj_3d& v) const {
+        o.pack_array(3);
+        o.pack(v.vertices);
+        o.pack(v.normals);
+        o.pack(v.faces);
+        return o;
+    }
+};
+
+template <>
+struct convert<benchmarking::test_obj3d::obj_3d> {
+    msgpack::object const& operator()(msgpack::object const& o, benchmarking::test_obj3d::obj_3d& v) const {
+        if (o.type != msgpack::type::ARRAY) { throw msgpack::type_error(); }
+        if (o.via.array.size != 3) { throw msgpack::type_error(); }
+        v.vertices = o.via.array.ptr[0].as<std::vector<benchmarking::test_obj3d::vertex>>();
+        v.normals = o.via.array.ptr[1].as<std::vector<benchmarking::test_obj3d::normal>>();
+        v.faces = o.via.array.ptr[2].as<std::vector<benchmarking::test_obj3d::face>>();
+        return o;
+    }
+};
+
+} // namespace adaptor
+} // MSGPACK_API_VERSION_NAMESPACE
+} // namespace msgpack
+
+static void bm_serialization(benchmark::State& state) {
+    auto data_out = benchmarking::test_obj3d::generate_benchmark_data();
+    msgpack::sbuffer buffer_out;
+    
+    for (auto _ : state) {
+        state.PauseTiming();
+        buffer_out.clear();
+        state.ResumeTiming();
+        msgpack::pack(buffer_out, data_out);
+    }
+}
+
+static void bm_deserialization(benchmark::State& state) {
+    auto buffer_in = []() -> msgpack::sbuffer {
+        msgpack::sbuffer buffer;
+        msgpack::pack(buffer, benchmarking::test_obj3d::generate_benchmark_data());
+        return buffer;
+    }();
+    benchmarking::test_obj3d::obj_3d data_in;
+    
+    for (auto _ : state) {
+        msgpack::object_handle obj_handle = msgpack::unpack(buffer_in.data(), buffer_in.size());
+        msgpack::object obj = obj_handle.get();
+        obj.convert(data_in);
+    }
+}
+
+BENCHMARK(bm_serialization)->MinWarmUpTime(10)->Repetitions(10);
+BENCHMARK(bm_deserialization)->MinWarmUpTime(10)->Repetitions(10);
+BENCHMARK_MAIN();
--- benchmarking/src/test_obj3d/benchmark_teg_compact/obj3d_teg_compact.cc
+++ benchmarking/src/test_obj3d/benchmark_teg_compact/obj3d_teg_compact.cc
@@ -7,20 +7,22 @@ static void bm_serialization(benchmark::State& state) {
     teg::byte_array buffer_out;
 
     for (auto _ : state) {
-        teg::serialize(buffer_out, data_out).or_throw();
+        teg::serialize<teg::compact_mode>(buffer_out, data_out).or_throw();
     }
+
+    std::cout << buffer_out.size() << std::endl;
 }
 
 static void bm_deserialization(benchmark::State& state) {    
     auto buffer_in = []() -> teg::byte_array {
         teg::byte_array buffer;
-        teg::serialize(buffer, benchmarking::test_obj3d::generate_benchmark_data()).or_throw();
+        teg::serialize<teg::compact_mode>(buffer, benchmarking::test_obj3d::generate_benchmark_data()).or_throw();
         return buffer;
     }();
     benchmarking::test_obj3d::obj_3d data_in;
 
     for (auto _ : state) {
-        teg::deserialize(buffer_in, data_in).or_throw();
+        teg::deserialize<teg::compact_mode>(buffer_in, data_in).or_throw();
     }
 }
 
