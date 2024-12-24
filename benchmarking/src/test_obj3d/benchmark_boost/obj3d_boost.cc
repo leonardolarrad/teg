9c5a7dcb4d2c41318661ce437ed547e56a8a7532
Leonardo Larrad <leonardolarrad@gmail.com>
2024-12-24 16:26:43 -0400

    Add benchmark `obj3d_boost`

---
+++ benchmarking/src/test_obj3d/benchmark_boost/obj3d_boost.cc
@@ -0,0 +1,7 @@
+#include "teg/teg.h"
+#include "benchmarking/test_obj3d.h"
+#include "benchmark/benchmark.h" // Google benchmark
+
+#include "boost/archive/binary_iarchive.hpp"
+#include "boost/archive/binary_oarchive.hpp"
+#include "boost/serialization/vector.hpp"
